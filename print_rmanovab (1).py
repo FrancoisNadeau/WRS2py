ccd16ad5aed005608f5ecdffb2a702aeaeca8855
FrancoisNadeau <45373513+FrancoisNadeau@users.noreply.github.com>
2024-04-16 09:09:04 -0400

    Delete print_rmanovab (1).py

--- print_rmanovab (1).py
+++
@@ -1,8 +0,0 @@
-def print_rmanovab(x, *args):
-    print("Call:")
-    print(x['call'])
-    print("\nTest statistic:", round(x['test'], 4))
-    print("Critical value:", round(x['crit'], 4))
-    sig = False if x['test'] < x['crit'] else True
-    print("Significant:", sig)
-    print("\n")
